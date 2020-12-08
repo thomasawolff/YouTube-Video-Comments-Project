f95959032b4f735de2a83ee78f0ac81ad3b0d46e
Thomas Wolff <datawolff@gmail.com>
2020-12-07 18:37:38 -0700

    Add files via upload

--- functionCalls.py
+++ functionCalls.py
@@ -1,11 +1,12 @@
 from sentenceVectDBConnect import *
 
 
-go = commentVectors(3,10000,10) # calling plot_similarity() method for K means cluster 3, 
-                                # over 10,000 samples from textAnalytics class, 
-                                # sampling 10 comments from those 10,000 samples
+##go = commentVectors(3,10000,10)
+##if __name__ == '__main__':
+##    Pool(go.plot_similarity())
+
 if __name__ == '__main__':
-    Pool(go.plot_similarity())
+    Pool(modelPredictionsLR('test'))
 
 
 #modelPredictionsLR()
