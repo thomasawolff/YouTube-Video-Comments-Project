6460b411f99c1cdf598d5ed6188fa909308ab329
Thomas Wolff <datawolff@gmail.com>
2020-12-11 07:21:40 -0700

    Add files via upload

--- functionCalls.py
+++ functionCalls.py
@@ -2,7 +2,7 @@ from sentenceVectConnect2 import *
 
 url = (r'C:\Users\moose_f8sa3n2\Google Drive\Research Methods\Course Project\YouTube Data\Unicode Files\youTubeVideosUTF.csv')
 
-go = textAnalytics(url,numClusters=4,cluster=3,category='Science & Technology')
+go = textAnalytics(url,numClusters=4,cluster=3,category='Music')
 #go.kMeansElbow()
 go.kMeansVisualizer()
 go.wordCloudVisualizer()
